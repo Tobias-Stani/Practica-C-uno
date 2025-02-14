4225cdf2a246b751550df5ee35831fe5e27affc4
Tobi-Stani <stanislavskytobias@gmail.com>
2025-02-13 21:12:57 -0300

    agregado primer ejercicio

---
+++ finalII/guiaCuatro/ej1.c
@@ -0,0 +1,28 @@
+//Rellene un array con los números pares comprendidos entre 1 y 100 y muéstrelos en 
+//pantalla en orden ascendente.
+
+int main () {
+
+    int arr[50], index = 0;
+
+    for (int i = 2; i <= 100; i+=2)
+    {
+        arr[index] = i;
+        index++;
+    }
+
+    printf("Numeros pares en orden ascendente:\n");
+    for (int i = 0; i < 50; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    
+
+    printf("Numeros pares en orden descendente:\n");
+    for (int i = 49; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+    
+
+}
